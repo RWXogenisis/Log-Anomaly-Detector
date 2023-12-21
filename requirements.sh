e33c8411c4df2da858c0adb22cedf97b0e820344
Hareesh S <105427651+HarSen0604@users.noreply.github.com>
2023-12-21 21:53:31 +0530

    To install the dependencies
    The script is needed to be run only for the first time.

---
+++ requirements.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Install required Python packages
+pip3 install rake-nltk
+pip3 install summa
+pip3 install nltk
+pip3 install gensim
+pip3 install pandas
+pip3 install Counter
+pip3 install keywords
+pip3 install BeautifulSoup4
+pip3 install lxml
+pip3 install pandas
+pip3 install joblib
+pip3 install scikit-learn
+
+# Make it executable by running the following command in the terminal: `chmod +x requirements.sh`
+# To run the script: `./requirements.sh`
